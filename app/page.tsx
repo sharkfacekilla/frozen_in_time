1b905e30e842d7acc164b87e82f1eb6b1f43c22d
sharkfacekilla <sharkfacekilla@gmail.com>
2024-10-24 16:44:42 -0700

    bg img lol

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
-    return (
+  return (
         <>
-            <h1>YEET</h1>
+            <section className="h-screen/2 bg-gradient-to-r from-black to-transparent">
+                <div className="inset-0 h-screen/2 hero h-svh">
+                    <div className="container grid grid-cols-2 content-center grid-rows-2 mx-auto px-4 h-full flex items-center justify-center">
+                        <h1 className="text-black font-extrabold md:text-8xl text-5xl sm:text-3xl text-light-blue z-10 mt-auto py-4 col-span-full">Web Site Name Here?</h1>
+                    </div>
+                </div>
+            </section>
         </>
-    );
+  );
 }
